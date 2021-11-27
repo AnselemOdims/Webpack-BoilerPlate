f2cb18d86516cdc82f8aba98147157d360cf60a3
Anselem <anselemodims@gmail.com>
2021-11-27 12:00:39 +0100

    Added another JS file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Avatar from './Avatar.jpg';
+import helper from './helper';
 
 function component(){
   let div = document.createElement('div')
@@ -9,6 +10,10 @@ function component(){
   let img = document.createElement('img');
   img.src = Avatar;
   div.append(img)
+  let btn = document.createElement('button');
+  btn.addEventListener('click', helper)
+  btn.innerHTML = 'Click Me!';
+  div.append(btn)
   return div
 }
 

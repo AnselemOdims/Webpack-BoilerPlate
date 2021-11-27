586f78cb279724aa573cad151d4db2936ae6314a
Anselem <anselemodims@gmail.com>
2021-11-27 11:46:04 +0100

    Included an image file in the document

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './style.css';
+import Avatar from './Avatar.jpg';
 
 function component(){
+  let div = document.createElement('div')
   let element = document.createElement('h1');
   element.textContent = 'Hello World!, still keeping'
-  return element
+  div.append(element)
+  let img = document.createElement('img');
+  img.src = Avatar;
+  div.append(img)
+  return div
 }
 
 document.querySelector('body').append(component())
